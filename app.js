1dfef5759282af4129589fd22e5818c7401f9385
aeriallistique <67158519+aeriallistique@users.noreply.github.com>
2021-02-21 17:17:05 +0000

    Update app.js

--- app.js
+++ app.js
@@ -432,14 +432,14 @@ document.body.addEventListener("touchstart", function (e) {
     if (e.target == canvas) {
       e.preventDefault();
     }
-  }, false);
+  });
   document.body.addEventListener("touchend", function (e) {
     if (e.target == canvas) {
       e.preventDefault();
     }
-  }, false);
+  });
   document.body.addEventListener("touchmove", function (e) {
     if (e.target == canvas) {
       e.preventDefault();
     }
-  }, false);
+  });
