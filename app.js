1b50eb9dede272d95ce1646a7a567c0431859884
aeriallistique <67158519+aeriallistique@users.noreply.github.com>
2021-02-26 16:55:14 +0000

    Update app.js
    ADDED CODE TO make canvas drawing work on mobile decives

--- app.js
+++ app.js
@@ -401,8 +401,6 @@ function handleDrawing (){
     canvas.width = drawing_content.clientWidth;
     let painting = false;
 
-
-
     window.addEventListener('resize', onScreenResize)
 
     function startPos (e){
@@ -424,10 +422,143 @@ function handleDrawing (){
         ctx.beginPath()
         ctx.moveTo(e.clientX, e.clientY - 90);
         
-    }
-
-    
+    }  
 }
+
+// ----- D R A W I N G   FOR   M O B I L E-------------
+function startup() {
+    var el = document.getElementById("canvas");
+    el.addEventListener("touchstart", handleStart, false);
+    el.addEventListener("touchend", handleEnd, false);
+    el.addEventListener("touchcancel", handleCancel, false);
+    el.addEventListener("touchmove", handleMove, false);
+  }
+  
+  document.addEventListener("DOMContentLoaded", startup);
+
+  var ongoingTouches = [];
+  var colorChoice = colorForTouch();
+
+  function handleStart(evt) {
+    evt.preventDefault();
+    var el = document.getElementById("canvas");
+    var ctx = el.getContext("2d");
+    var touches = evt.changedTouches;
+    
+    for (var i = 0; i < touches.length; i++) {
+      ongoingTouches.push(copyTouch(touches[i]));
+      ctx.beginPath();
+      ctx.lineTo(evt.pageX, evt.pageY - 150, 8, 0, 2 * Math.PI, false);  // a circle at the start
+      ctx.lineWidth = 200;
+      ctx.lineCap = 'round';
+      ctx.strokeStyle = colorChoice;
+      ctx.fill();
+     
+    }
+  }
+
+
+
+  function handleMove(evt) {
+    evt.preventDefault();
+    var el = document.getElementById("canvas");
+    var ctx = el.getContext("2d");
+    var touches = evt.changedTouches;
+  
+    for (var i = 0; i < touches.length; i++) {
+      
+      var idx = ongoingTouchIndexById(touches[i].identifier);
+  
+      if (idx >= 0) {
+        ctx.beginPath();
+        ctx.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY - 150);
+        ctx.lineTo(touches[i].pageX, touches[i].pageY - 150);
+        ctx.lineWidth = 12;
+        ctx.lineCap = 'round'
+        ctx.strokeStyle = colorChoice;
+        ctx.stroke();
+  
+        ongoingTouches.splice(idx, 1, copyTouch(touches[i]));  // swap in the new touch record
+      } else {
+        console.log("can't figure out which touch to continue");
+      }
+    }
+  }
+
+
+
+  function handleEnd(evt) {
+    evt.preventDefault();
+    
+    var el = document.getElementById("canvas");
+    var ctx = el.getContext("2d");
+    var touches = evt.changedTouches;
+  
+    for (var i = 0; i < touches.length; i++) {
+      var idx = ongoingTouchIndexById(touches[i].identifier);
+  
+      if (idx >= 0) {
+        ctx.lineWidth = 12;
+        ctx.lineCap = 'round';
+        ctx.fillStyle = colorChoice;
+        ctx.beginPath();
+        ctx.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
+        ctx.lineTo(touches[i].pageX, touches[i].pageY);
+
+        //ctx.fillRect(touches[i].pageX - 4, touches[i].pageY - 4, 8, 8);  // and a square at the end
+
+        ctx.arc(touches[i].pageX, touches[i].pageY, 12, 0, 2 * Math.PI, false);  // a circle at the end
+
+        ongoingTouches.splice(idx, 1);  // remove it; we're done
+      } else {
+        console.log("can't figure out which touch to end");
+      }
+    }
+  }
+
+
+
+  function handleCancel(evt) {
+    evt.preventDefault();
+    console.log("touchcancel.");
+    var touches = evt.changedTouches;
+  
+    for (var i = 0; i < touches.length; i++) {
+      var idx = ongoingTouchIndexById(touches[i].identifier);
+      ongoingTouches.splice(idx, 1);  // remove it; we're done
+    }
+  }
+
+
+  function colorForTouch() {
+    let colorArray = ['blue', 'red', 'green', 'pink', 'violet', 'yellow', 'purple', 'gray'];
+    let index = Math.floor(Math.random() * colorArray.length);
+    let color = colorArray[index];
+   // log(color);
+    return color;
+  }
+
+
+  function copyTouch({ identifier, pageX, pageY }) {
+    return { identifier, pageX, pageY };
+  }
+
+
+  function ongoingTouchIndexById(idToFind) {
+    for (var i = 0; i < ongoingTouches.length; i++) {
+      var id = ongoingTouches[i].identifier;
+  
+      if (id == idToFind) {
+        return i;
+      }
+    }
+    return -1;    // not found
+  }
+
+  function log(msg) {
+    var p = document.getElementById('log');
+    p.innerHTML = msg + "\n" + p.innerHTML;
+  }
 
 
 
